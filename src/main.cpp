5fa864a95b50baebabebb58842187235e3aa20bd
Markku Korsumäki <markku.korsumaki@gmail.com>
2026-03-21 00:00:32 +0200

    Add SSD1306 display

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,122 @@
 #include <scheduler.h>
 #include <temperature_sensor.h>
 #include <humidity_sensor.h>
+#include <screen.h> // Screen content handling
 
-int blink_task(void)
+
+// Display: ESP8266 and ESP32 OLED driver for SSD1306 displays 
+// 2x I2C Button (customizable I2C Address, default:0x31)
+#include <LOLIN_I2C_BUTTON.h>
+#include "SSD1306Wire.h"
+
+// Initialize the OLED display using Wire library
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
+
+I2C_BUTTON button(DEFAULT_I2C_BUTTON_ADDRESS);
+
+bool is_display_available = false;
+
+void init_display(void)
 {
-    static bool is_led_on = false;
-    int time_for_next_call = 0;
-
-    is_led_on = !is_led_on;
-    if (is_led_on)
+    // Init display
+    is_display_available = display.init();
+    if (is_display_available)
     {
-        digitalWrite(LED_BUILTIN, LOW);
-        Serial.println("led on");
-        time_for_next_call = 50;
-    }
-    else
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        Serial.println("led off");
-        time_for_next_call = 500;
-    }
+        display.flipScreenVertically();
+        display.setContrast(255);
 
-    return time_for_next_call;
+        display.println("IoT mesh\n");
+    }
 }
+
+uint8_t screen_page_index = 0;
+uint8_t screen_scroll_index = 0;
+
+void button_a_pressed(void)
+{
+    screen_page_index++;
+    screen_scroll_index = 0;
+}
+
+void button_b_pressed(void)
+{
+    screen_scroll_index++;
+}
+
+void display_loop(void)
+{
+    if (is_display_available)
+    {
+        display.cls();
+        if (screen_page_index >= screen_get_count())
+        {
+            screen_page_index = 0;
+        }
+        display.printf("%s", screen_get_screen(screen_page_index));
+        //display.println("IoT mesh 2");
+        //display.printf("%d %d\n", screen_page_index, screen_scroll_index);
+    }
+}
+
+
+
+void handle_buttons(void)
+{
+    if (button.get() == 0)
+    {
+        if (button.BUTTON_A == KEY_VALUE_SHORT_PRESS)
+        {
+            Serial.println("button A");
+            button_a_pressed();
+        }
+
+        if (button.BUTTON_B == KEY_VALUE_SHORT_PRESS)
+        {
+            Serial.println("button B");
+            button_b_pressed();
+        }
+    }
+}
+
+
+const char * screen1(void)
+{
+    return "Temperature\n12.4 C";
+}
+const char * screen2(void)
+{
+    return "Humidity\n65%%";
+}
+const char * screen3(void)
+{
+    return "Mesh";
+}
+
 
 void setup()
 {
     Serial.begin(115200);
     (void)scheduler_loop(millis());
 
-    temperature_sensor_init();
-    humidity_sensor_init();
+    init_display();
+    screen_init();
+    screen_add_screen(screen1);
+    screen_add_screen(screen2);
+    screen_add_screen(screen3);
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+    //temperature_sensor_init();
+    //humidity_sensor_init();
 
     //scheduler_add_task(blink_task, 0);
 
     //temperature_sensor_measure();
-    humidity_sensor_measure();
+    //humidity_sensor_measure();
 }
 
 void loop()
 {
+    handle_buttons();
+    display_loop();
+    delay(200);
     scheduler_loop(millis());
 }
