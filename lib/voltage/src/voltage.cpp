ba17d7d34f66a7ac8760a7baaf97a928e751175a
Markku Korsumäki <markku.korsumaki@gmail.com>
2026-03-30 22:33:52 +0300

    Fix voltage calculation to include wemos adc internal resistors

--- lib/voltage/src/voltage.cpp
+++ lib/voltage/src/voltage.cpp
@@ -4,8 +4,17 @@
 
 
 // MKO-01 rev0
-#define VOLTAGE_MEAS_UPPER_RESISITOR (470.0f) // 470k
-#define VOLTAGE_MEAS_LOWER_RESISITOR (100.0f) // 100k
+#define RES_MKO_01_UPPER_RESISITOR (470.0f) // 470k
+#define RES_MKO_01_LOWER_RESISITOR (100.0f) // 100k (parallel with Wemos adc internal resistors)
+
+// Wemos d1 mini (internal adc resistors)
+#define RES_WEMOS_ADC_RESISTOR (220.0f+100.0f) // 220k + 100k
+
+// Combined lower resistor: MKO-01 & Wemos parallel resistors
+#define RES_COMBINED_LOWER_RESISITOR (1.0f / ((1.0f/RES_WEMOS_ADC_RESISTOR) + (1.0f/RES_MKO_01_LOWER_RESISITOR)))
+
+#define VOLTAGE_MEAS_UPPER_RESISITOR    RES_MKO_01_UPPER_RESISITOR
+#define VOLTAGE_MEAS_LOWER_RESISITOR    RES_COMBINED_LOWER_RESISITOR
 #define VOLTAGE_MEAS_ADC_PIN         (A0)
 
 
@@ -23,8 +32,8 @@ float voltage_get(void)
 static int voltage_meas_task(void)
 {
     // Constants
-    const float V_REF = 3.3;     // Analog reference voltage (e.g., 5V or 3.3V)
-    const float R_BITS = 10.0;   // ADC resolution (bits)
+    const float V_REF = 3.2f;   // Analog reference voltage (e.g., 5V or 3.3V)
+    const float R_BITS = 10;    // ADC resolution (bits)
     const float ADC_STEPS = (1 << int(R_BITS)) - 1; // Number of steps (2^R_BITS - 1)
 
     /*int*/ adc_value = analogRead(VOLTAGE_MEAS_ADC_PIN);
